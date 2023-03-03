a2daaaa72641ba54bbf1b831ab0168a92ab31974
Robert Friel <rob@rungalileo.io>
2023-03-02 22:57:32 -0500

    remove fairscale stuff

--- example.py
+++ example.py
@@ -17,17 +17,18 @@ from llama import ModelArgs, Transformer, Tokenizer, LLaMA
 
 
 def setup_model_parallel() -> Tuple[int, int]:
-    local_rank = int(os.environ.get("LOCAL_RANK", 0))
-    world_size = int(os.environ.get("WORLD_SIZE", 1))
+    return 0, 1
+    # local_rank = int(os.environ.get("LOCAL_RANK", 0))
+    # world_size = int(os.environ.get("WORLD_SIZE", 1))
 
-    torch.distributed.init_process_group(
-        "nccl", init_method='file:///foobar', world_size=1, rank=0)
-    initialize_model_parallel(world_size)
-    torch.cuda.set_device(local_rank)
+    # torch.distributed.init_process_group(
+    #     "nccl", init_method='file:///foobar', world_size=1, rank=0)
+    # initialize_model_parallel(world_size)
+    # torch.cuda.set_device(local_rank)
 
-    # seed must be the same in all processes
-    torch.manual_seed(1)
-    return local_rank, world_size
+    # # seed must be the same in all processes
+    # torch.manual_seed(1)
+    # return local_rank, world_size
 
 
 def load(ckpt_dir: str, tokenizer_path: str, local_rank: int, world_size: int) -> LLaMA:
@@ -57,9 +58,9 @@ def load(ckpt_dir: str, tokenizer_path: str, local_rank: int, world_size: int) -
 
 
 def main(ckpt_dir: str, tokenizer_path: str, temperature: float = 0.8, top_p: float = 0.95):
-    local_rank, world_size = setup_model_parallel()
-    if local_rank > 0:
-        sys.stdout = open(os.devnull, 'w')
+    # local_rank, world_size = setup_model_parallel()
+    # if local_rank > 0:
+        # sys.stdout = open(os.devnull, 'w')
 
     generator = load(ckpt_dir, tokenizer_path, local_rank, world_size)
     prompts = ["The capital of Germany is the city of", "Here is my sonnet in the style of Shakespeare about an artificial intelligence:"]
--- llama/model.py
+++ llama/model.py
@@ -11,9 +11,9 @@ import torch.nn.functional as F
 
 import fairscale.nn.model_parallel.initialize as fs_init
 from fairscale.nn.model_parallel.layers import (
-    ParallelEmbedding,
-    RowParallelLinear,
-    ColumnParallelLinear,
+    nn.Embedding,
+    nn.Linear,
+    nn.Linear,
 )
 
 
@@ -80,28 +80,28 @@ class Attention(nn.Module):
         self.n_local_heads = args.n_heads // fs_init.get_model_parallel_world_size()
         self.head_dim = args.dim // args.n_heads
 
-        self.wq = ColumnParallelLinear(
+        self.wq = nn.Linear(
             args.dim,
             args.n_heads * self.head_dim,
             bias=False,
             gather_output=False,
             init_method=lambda x: x,
         )
-        self.wk = ColumnParallelLinear(
+        self.wk = nn.Linear(
             args.dim,
             args.n_heads * self.head_dim,
             bias=False,
             gather_output=False,
             init_method=lambda x: x,
         )
-        self.wv = ColumnParallelLinear(
+        self.wv = nn.Linear(
             args.dim,
             args.n_heads * self.head_dim,
             bias=False,
             gather_output=False,
             init_method=lambda x: x,
         )
-        self.wo = RowParallelLinear(
+        self.wo = nn.Linear(
             args.n_heads * self.head_dim,
             args.dim,
             bias=False,
@@ -169,13 +169,13 @@ class FeedForward(nn.Module):
         hidden_dim = int(2 * hidden_dim / 3)
         hidden_dim = multiple_of * ((hidden_dim + multiple_of - 1) // multiple_of)
 
-        self.w1 = ColumnParallelLinear(
+        self.w1 = nn.Linear(
             dim, hidden_dim, bias=False, gather_output=False, init_method=lambda x: x
         )
-        self.w2 = RowParallelLinear(
+        self.w2 = nn.Linear(
             hidden_dim, dim, bias=False, input_is_parallel=True, init_method=lambda x: x
         )
-        self.w3 = ColumnParallelLinear(
+        self.w3 = nn.Linear(
             dim, hidden_dim, bias=False, gather_output=False, init_method=lambda x: x
         )
 
@@ -210,7 +210,7 @@ class Transformer(nn.Module):
         self.vocab_size = params.vocab_size
         self.n_layers = params.n_layers
 
-        self.tok_embeddings = ParallelEmbedding(
+        self.tok_embeddings = nn.Embedding(
             params.vocab_size, params.dim, init_method=lambda x: x
         )
 
@@ -219,7 +219,7 @@ class Transformer(nn.Module):
             self.layers.append(TransformerBlock(layer_id, params))
 
         self.norm = RMSNorm(params.dim, eps=params.norm_eps)
-        self.output = ColumnParallelLinear(
+        self.output = nn.Linear(
             params.dim, params.vocab_size, bias=False, init_method=lambda x: x
         )
 
