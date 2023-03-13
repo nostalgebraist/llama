c266b85d494ac33dc08a8dd4cde7a47bc4e3340d
nostalgebraist <nostalgebraist@gmail.com>
2023-03-13 14:00:36 -0700

    generation code with breakruns

---
+++ llama/breakruns.py
@@ -0,0 +1,133 @@
+import torch
+
+class BreakrunsLogitsProcessor:
+    def __init__(self,
+                base_temperature: float,
+                tau: float,
+                debug=True,
+                tokenizer=None,
+                first_count=0,
+                disable_trigger = None,
+                enable_trigger = None,
+                modify_on_trigger = None,
+                modify_off_trigger = None,
+                temp_shift_modifier = 0.0,
+                device='cuda:0',
+                ):
+        self.base_temperature = base_temperature
+        self.tau = tau
+        self.debug = debug
+        self.tokenizer = tokenizer
+        self.first_count = first_count
+        self.disable_trigger = None if disable_trigger is None else torch.as_tensor(disable_trigger)[None, :].to(device)
+        self.enable_trigger = None if enable_trigger is None else torch.as_tensor(enable_trigger)[None, :].to(device)
+        self.modify_on_trigger = None if modify_on_trigger is None else torch.as_tensor(modify_on_trigger)[None, :].to(device)
+        self.modify_off_trigger = None if modify_off_trigger is None else torch.as_tensor(modify_off_trigger)[None, :].to(device)
+        self.temp_shift_modifier = temp_shift_modifier
+
+        self.enabled = None
+        self.modified = None
+        self.breakruns_counter = None
+        self.last_logits = None
+        self.last_length = None
+        self.first_length = None
+        self.temp_avg = None
+        self.max_run_len = None
+
+    def _reset(self):
+        self._dprint("BREAKRUNS: _reset")
+        self.enabled = None
+        self.modified = None
+        self.breakruns_counter = None
+        self.last_logits = None
+        self.temp_avg = None
+        self.max_run_len = None
+
+    def _dprint(self, msg, fillers={}, **kwargs):
+        if self.debug:
+            print(msg.format(**fillers), **kwargs)
+
+    def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor) -> torch.FloatTensor:
+        seq_length = input_ids.shape[1]
+        if seq_length < 1:
+            self._dprint("BREAKRUNS: empty sequence, no op")
+            return scores
+
+        if self.last_length is None or self.last_length > input_ids.shape[1]:
+            # new sequence
+            self._reset()
+        self.last_length = input_ids.shape[1]
+        if self.first_length is None:
+            self.first_length = self.last_length
+
+        if self.enabled is None:
+            self._dprint("BREAKRUNS: init enabled")
+            self.enabled = torch.ones((input_ids.shape[0],), device=input_ids.device)
+
+        if self.modified is None:
+            self._dprint("BREAKRUNS: init modified")
+            self.modified = torch.zeros((input_ids.shape[0],), device=input_ids.device)
+
+        if self.breakruns_counter is None:
+            self.breakruns_counter = self.first_count * torch.ones((), device=input_ids.device)
+            self.temp_avg = torch.zeros((), device=input_ids.device)
+            self.max_run_len = torch.zeros((), device=input_ids.device, dtype=self.breakruns_counter.dtype)
+            self._dprint(f"BREAKRUNS: init counter {self.breakruns_counter}")
+
+        if self.last_logits is None:
+            self._dprint("BREAKRUNS: init logits, no op")
+            self.last_logits = scores
+
+            return scores
+
+        if (self.disable_trigger is not None) and (self.enable_trigger is not None):
+            with torch.no_grad():
+                prev_enabled = self.enabled
+
+                disable_flip = (input_ids[:, -self.disable_trigger.shape[1]:] == self.disable_trigger.to(input_ids.device)).all(dim=1)
+                enable_flip = (input_ids[:, -self.enable_trigger.shape[1]:] == self.enable_trigger.to(input_ids.device)).all(dim=1)
+
+                self.enabled = torch.logical_or(enable_flip, self.enabled)
+                self.enabled = torch.logical_and(~disable_flip, self.enabled)
+
+                if self.debug and (self.enabled != prev_enabled).any().item():
+                    self._dprint(f"BREAKRUNS: enabled {prev_enabled} -> {self.enabled}")
+
+        # TODO: DRY
+        if (self.modify_on_trigger is not None) and (self.modify_off_trigger is not None):
+            with torch.no_grad():
+                prev_modified = self.modified
+
+                disable_flip = (input_ids[:, -self.modify_off_trigger.shape[1]:] == self.modify_off_trigger.to(input_ids.device)).all(dim=1)
+                enable_flip = (input_ids[:, -self.modify_on_trigger.shape[1]:] == self.modify_on_trigger.to(input_ids.device)).all(dim=1)
+
+                self._dprint(f"modify on: {input_ids[:, -self.modify_on_trigger.shape[1]:]} vs {self.modify_on_trigger}")
+
+                # note reversed order
+                self.modified = torch.logical_and(~disable_flip, self.modified)
+                self.modified = torch.logical_or(enable_flip, self.modified)
+
+                if self.debug and (self.modified != prev_modified).any().item():
+                    self._dprint(f"BREAKRUNS: modified {prev_modified} -> {self.modified}")
+
+        # check if last was top
+        was_top = (input_ids[:, -1] == self.last_logits.argmax(dim=1)).to(torch.long)
+
+        self.breakruns_counter = was_top * self.enabled * (self.breakruns_counter + 1)
+
+        self.max_run_len = torch.max(self.max_run_len, self.breakruns_counter)
+
+        if self.debug:
+            sampled_str = repr(self.tokenizer.decode(input_ids[0, -1].item()))
+            actual_top_str = repr(self.tokenizer.decode([self.last_logits.argmax(dim=1)[0].item()]))
+            print(f"sampled {sampled_str} actual_top {actual_top_str} | enabled {self.enabled[0]} | modified {self.modified[0]} | was_top?: {was_top[0]} | self.breakruns_counter: {self.breakruns_counter}")
+
+        eff_temperature = self.base_temperature + (self.breakruns_counter * self.tau) + (self.modified * self.temp_shift_modifier)
+        
+        avg_r = 1./(self.last_length - self.first_length)
+        self.temp_avg = (1-avg_r)*self.temp_avg + avg_r*eff_temperature
+        self._dprint("eff_temperature:\n{et}", fillers={"et": eff_temperature.item() if len(eff_temperature)==1 else eff_temperature})
+
+        self.last_logits = scores
+
+        return scores / eff_temperature[:, None].expand_as(scores)
--- llama/generation.py
+++ llama/generation.py
@@ -4,9 +4,18 @@
 from typing import List
 
 import torch
+from tqdm.auto import tqdm, trange
 
 from llama.tokenizer import Tokenizer
 from llama.model import Transformer
+from llama.breakruns import BreakrunsLogitsProcessor
+
+
+def xformers_off(m):
+    m.use_xformers=False
+
+def xformers_on(m):
+    m.use_xformers=True
 
 
 class LLaMA:
@@ -20,6 +29,11 @@ class LLaMA:
         max_gen_len: int,
         temperature: float = 0.8,
         top_p: float = 0.95,
+        stop_at_eos=True,
+        allow_xformers=False,
+        breakruns=True,
+        breakruns_tau=0.035,
+        debug=False,
     ) -> List[str]:
         bsz = len(prompts)
         params = self.model.params
@@ -38,8 +52,26 @@ class LLaMA:
         input_text_mask = tokens != self.tokenizer.pad_id
         start_pos = min_prompt_size
         prev_pos = 0
-        for cur_pos in range(start_pos, total_len):
+
+        lp = None
+        if breakruns:
+            lp = BreakrunsLogitsProcessor(base_temperature=temperature, 
+                                        tau=breakruns_tau, 
+                                        tokenizer=self.tokenizer,
+                                        debug=debug)
+            temperature = 1.0
+
+        for cur_pos in trange(start_pos, total_len, mininterval=0.25):
+            if allow_xformers and cur_pos - prev_pos > 1:
+                self.model.apply(xformers_on);
+            else:
+                self.model.apply(xformers_off);
+
             logits = self.model.forward(tokens[:, prev_pos:cur_pos], prev_pos)[:, -1, :]
+
+            if lp is not None:
+                logits = lp(tokens[:, start_pos:cur_pos], logits)
+
             if temperature > 0:
                 probs = torch.softmax(logits / temperature, dim=-1)
                 next_token = sample_top_p(probs, top_p)
@@ -52,18 +84,34 @@ class LLaMA:
             )
             tokens[:, cur_pos] = next_token
             prev_pos = cur_pos
+            if stop_at_eos and bsz == 1 and (next_token == self.tokenizer.eos_id):
+                break
 
         decoded = []
+        if breakruns:
+            temp_avg = lp.temp_avg.item() if len(lp.temp_avg) == 1 else lp.temp_avg.cpu().numpy()
+            print(f"temp_avg: {temp_avg}")
+            if lp.tau > 0:
+                counter_avg = (temp_avg - lp.base_temperature) / lp.tau
+                print(f"counter_avg: {counter_avg}")
+
+            max_run_len = lp.max_run_len.item() if len(lp.max_run_len) == 1 else lp.max_run_len.cpu().numpy()
+            print(f"max_run_len: {max_run_len}")
+
         for i, t in enumerate(tokens.tolist()):
             # cut to max gen len
             t = t[: len(prompt_tokens[i]) + max_gen_len]
-            # cut to eos tok if any
             try:
-                t = t[: t.index(self.tokenizer.eos_id)]
+                t = t[: t.index(-1)]
             except ValueError:
                 pass
-            decoded.append(self.tokenizer.decode(t))
+            try:
+                decoded.append(self.tokenizer.decode(t))
+            except Exception as e:
+                display(e)
+                return t
         return decoded
+
 
 
 def sample_top_p(probs, p):
